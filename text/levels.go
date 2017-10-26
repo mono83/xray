e6efe5851225a29d90594e0e34c3e4d8ad0d04cd
Gotterdemarung <gotterdemarung@gmail.com>
2017-10-26 15:40:03 +0300

    levels text

---
+++ text/levels.go
@@ -0,0 +1,50 @@
+package text
+
+import (
+	"github.com/mono83/xray"
+	"strings"
+)
+
+// LevelToString returns string representation of xray Level
+func LevelToString(l xray.Level) string {
+	switch l {
+	case xray.TRACE:
+		return "trace"
+	case xray.DEBUG:
+		return "debug"
+	case xray.INFO:
+		return "info"
+	case xray.WARNING:
+		return "warning"
+	case xray.ERROR:
+		return "error"
+	case xray.ALERT:
+		return "alert"
+	case xray.CRITICAL:
+		return "critical"
+	default:
+		return "unknown"
+	}
+}
+
+// ParseLevel parses string into xray Level
+func ParseLevel(v string) xray.Level {
+	switch strings.ToLower(v) {
+	case "trace":
+		return xray.TRACE
+	case "debug":
+		return xray.DEBUG
+	case "info":
+		return xray.INFO
+	case "warn", "warning":
+		return xray.WARNING
+	case "error":
+		return xray.ERROR
+	case "alert":
+		return xray.ALERT
+	case "critical", "emergency":
+		return xray.CRITICAL
+	default:
+		return xray.INFO
+	}
+}
