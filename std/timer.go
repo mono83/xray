607bb009755e6300bca3e074a75f83840dc79154
Gotterdemarung <gotterdemarung@gmail.com>
2017-10-30 10:28:31 +0200

    timer helper

---
+++ std/timer.go
@@ -0,0 +1,45 @@
+package std
+
+import (
+	"github.com/mono83/xray"
+	"time"
+)
+
+// TimeHelper is helper structure, used to measure elapsed time with automatic data forwarding
+// into associated ray
+type TimeHelper interface {
+	xray.NanoHolder
+	Stop() time.Duration
+}
+
+// Timer builds new timer (TimeHelper instance) used for time measurement
+func Timer(r xray.Ray, name string, args ...xray.Arg) TimeHelper {
+	return &timer{
+		start: time.Now(),
+		name:  name,
+		ray:   r,
+		args:  args,
+	}
+}
+
+type timer struct {
+	start, stop time.Time
+	name        string
+	ray         xray.Ray
+	args        []xray.Arg
+}
+
+func (t *timer) Stop() time.Duration {
+	if t.stop.IsZero() {
+		t.stop = time.Now()
+		if t.ray != nil {
+			t.ray.Duration(t.name, t.stop.Sub(t.start), t.args...)
+		}
+	}
+
+	return t.stop.Sub(t.start)
+}
+
+func (t *timer) Nanoseconds() int64 {
+	return t.Stop().Nanoseconds()
+}
---
+++ std/timer_test.go
@@ -0,0 +1,36 @@
+package std
+
+import (
+	"testing"
+	assert2 "github.com/stretchr/testify/assert"
+	"github.com/mono83/xray/args"
+)
+
+func TestTimer(t *testing.T) {
+	assert := assert2.New(t)
+
+	// Building timer object without ray
+	o := Timer(nil, "foo", args.Name("test"))
+	assert.NotNil(o)
+
+	// Cast
+	if x, ok := o.(*timer); ok {
+		// Initial state
+		assert.False(x.start.IsZero())
+		assert.True(x.stop.IsZero())
+
+		// Stopping
+		o.Stop()
+		assert.False(x.start.IsZero())
+		assert.False(x.stop.IsZero())
+
+		current := x.stop
+
+		// Double stop
+		o.Stop()
+		assert.Equal(current.UnixNano(), x.stop.UnixNano())
+		assert.True(o.Stop().Nanoseconds() > 0)
+	} else {
+		assert.Fail("Not a timer struct")
+	}
+}
