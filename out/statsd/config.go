03d49d92db5f162738452683aed4757c3c1ba585
Gotterdemarung <gotterdemarung@gmail.com>
2017-10-30 14:18:22 +0200

    statsd integration

--- out/statsd/config.go
+++ out/statsd/config.go
@@ -1,21 +1,19 @@
-package logstash
+package statsd
 
 import (
 	"errors"
 	"github.com/mono83/udpwriter"
 	"github.com/mono83/xray"
 	"github.com/mono83/xray/out"
-	"github.com/mono83/xray/text"
 	"net"
 	"time"
 )
 
-// Config holds information for filtered receiver
-// Tags for JSON, YAML and TOML are configured
+// Config holds configuration for StatsD client
 type Config struct {
 	Address       string   `json:"address" yaml:"address" toml:"address"`
-	MinLevel      string   `json:"level" yaml:"level" toml:"level"`
 	Buffer        int      `json:"bufferMillis" yaml:"bufferMillis" toml:"bufferMillis"`
+	Args          bool     `json:"args" yaml:"args" toml:"args"`
 	ArgsWhiteList []string `json:"argsWhiteList" yaml:"argsWhiteList" toml:"argsWhiteList"`
 	ArgsBlackList []string `json:"argsBlackList" yaml:"argsBlackList" toml:"argsBlackList"`
 }
@@ -23,7 +21,7 @@ type Config struct {
 // Validate validates configuration contents
 func (c Config) Validate() error {
 	if len(c.Address) == 0 {
-		return errors.New("empty Logstash binding address")
+		return errors.New("empty StatsD binding address")
 	}
 	if c.Buffer < 100 {
 		return errors.New("at least 100ms buffering must be configured")
@@ -32,41 +30,7 @@ func (c Config) Validate() error {
 	return nil
 }
 
-// MustBuild is an alias for Build but panics on error
-func (c Config) MustBuild() xray.Handler {
-	hld, err := c.Build()
-	if err != nil {
-		panic(err)
-	}
-
-	return hld
-}
-
-// Build builds asynchronous buffered logstash receiver with log-level filtering
-func (c Config) Build() (xray.Handler, error) {
-	hld, err := c.Raw()
-	if err != nil {
-		return nil, err
-	}
-
-	level := text.ParseLevel(c.MinLevel)
-
-	return out.Filter(
-		out.Buffer(
-			hld,
-			time.Duration(c.Buffer)*time.Millisecond,
-		),
-		func(event xray.Event) bool {
-			if event == nil {
-				return false
-			}
-			l, ok := event.(xray.LogEvent)
-			return ok && l.GetLevel() >= level
-		},
-	), nil
-}
-
-// Raw method builds synchronous sender for logstash.
+// Raw method builds synchronous sender for StatsD.
 // It is not recommended to use this method, call Build() instead
 func (c Config) Raw() (xray.Handler, error) {
 	if err := c.Validate(); err != nil {
@@ -80,9 +44,45 @@ func (c Config) Raw() (xray.Handler, error) {
 
 	// Building sender
 	send := &sender{
-		target:    udpwriter.New(netAddr),
-		argFilter: xray.ArgFilterDoubleList(c.ArgsWhiteList, c.ArgsBlackList),
+		target:     udpwriter.New(netAddr),
+		argAllowed: c.Args,
+		argFilter:  xray.ArgFilterDoubleList(c.ArgsWhiteList, c.ArgsBlackList),
 	}
 
 	return send.handle, nil
+}
+
+// MustBuild is an alias for Build but panics on error
+func (c Config) MustBuild() xray.Handler {
+	hld, err := c.Build()
+	if err != nil {
+		panic(err)
+	}
+
+	return hld
+}
+
+// Build builds asynchronous buffered StatsD receiver
+func (c Config) Build() (xray.Handler, error) {
+	hld, err := c.Raw()
+	if err != nil {
+		return nil, err
+	}
+
+	return out.Filter(
+		out.Buffer(
+			out.Splitter(
+				hld,
+				10,
+			),
+			time.Duration(c.Buffer)*time.Millisecond,
+		),
+		func(event xray.Event) bool {
+			if event == nil {
+				return false
+			}
+			_, ok := event.(xray.MetricsEvent)
+			return ok
+		},
+	), nil
 }
---
+++ std/sql.go
@@ -0,0 +1,74 @@
+package std
+
+import (
+	"github.com/mono83/xray"
+	"github.com/mono83/xray/args"
+	"strings"
+	"time"
+)
+
+// queryType function attempts to recognize SQL query type
+func queryType(sql string) string {
+	sql = strings.TrimSpace(sql)
+	if len(sql) < 6 {
+		return ""
+	}
+
+	switch sql[0:6] {
+	case "select":
+		return "select"
+	case "insert":
+		return "insert"
+	case "update":
+		return "update"
+	case "delete":
+		return "delete"
+	default:
+		return ""
+	}
+}
+
+// SQLLog command performs SQL logging
+func SQLLog(r xray.Ray, sql string, elapsed xray.NanoHolder, err error) {
+	if r == nil {
+		return
+	}
+
+	// Sending SQL latency
+	r.Duration("sql.latency", elapsed, args.SQL(sql))
+
+	list := []xray.Arg{args.SQL(sql), args.Delta(time.Duration(elapsed.Nanoseconds()))}
+	if t := queryType(sql); len(t) > 0 {
+		list = append(list, args.Type(t))
+	}
+
+	// Sending message
+	if err == nil {
+		r.Debug("Query :sql done in :delta", list...)
+	} else {
+		list = append(list, args.Error{Err: err})
+		r.Error("Query :sql done in :delta with error :err", list...)
+	}
+}
+
+// WrapSQLQuery method takes SQL reader function and returns wrapped one, that will
+// supply additional logging data into ray
+func WrapSQLQuery(r xray.Ray, f func(interface{}, string, ...interface{}) error) func(interface{}, string, ...interface{}) error {
+	if r == nil {
+		return f
+	}
+
+	return func(target interface{}, sql string, sqlargs ...interface{}) error {
+		// Starting timer
+		now := time.Now()
+
+		// Performing request
+		err := f(target, sql, sqlargs...)
+
+		// Logging
+		SQLLog(r, sql, time.Now().Sub(now), err)
+
+		// Response
+		return err
+	}
+}
