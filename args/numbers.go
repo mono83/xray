62bad7a862d3e9c2c45595169138ef9457302c76
Gotterdemarung <gotterdemarung@gmail.com>
2017-11-02 08:43:41 +0200

    id value argument

--- args/numbers.go
+++ args/numbers.go
@@ -17,6 +17,18 @@ func (i Int64) Value() string { return strconv.FormatInt(i.V, 10) }
 // Scalar is ray.Arg interface implementation. Returns argument value as scalar
 func (i Int64) Scalar() interface{} { return i.V }
 
+// ID64 is number argument with name "id"
+type ID64 int64
+
+// Name is ray.Arg interface implementation. Returns argument name
+func (ID64) Name() string { return "id" }
+
+// Value is ray.Arg interface implementation. Returns argument value
+func (i ID64) Value() string { return strconv.FormatInt(int64(i), 10) }
+
+// Scalar is ray.Arg interface implementation. Returns argument value as scalar
+func (i ID64) Scalar() interface{} { return int64(i) }
+
 // Count represents integer arg with name "count"
 type Count int
 
